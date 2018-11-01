0f8e919669d7f52b4d0af95ab01571e58c1aa20a
Brendan M <bjohnmurphy1984@gmail.com>
2018-11-01 11:22:01 -0400

    first few unit tests

--- src/test/java/cscie55/hw5/rentals/AccountTest.java
+++ src/test/java/cscie55/hw5/rentals/AccountTest.java
@@ -2,8 +2,36 @@ package cscie55.hw5.rentals;
 
 import org.junit.Test;
 
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class AccountTest {
+
+	//tests the account creation functionality
 	@Test
-	public void testAccount() {
+	public void testAccountCreation() {
+		Account brendanAccount = new Account("Brendan", "Murphy", "bMurphy@gmail.com");
+		assertEquals(brendanAccount.toString(),"Brendan Murphy bMurphy@gmail.com");
+	}
+
+	//tests that hasOpenRental returns true with a new rental
+	@Test
+	public void testAddRental() throws VideoException{
+		Account mikeAccount = new Account("Mike", "Murphy", "mMurphy@gmail.com");
+		Video forrestGump = new Video("Forrest Gump", 1995);
+		LocalDate dueDate = LocalDate.of(2018, 11, 20);
+		VideoRental rental = new VideoRental(forrestGump, mikeAccount,dueDate);
+		assertEquals(mikeAccount.getOpenRentals().size(), 1);
+	}
+
+	//tests that hasOpenRental returns false with no rentals
+	@Test
+	public void testNoOpenRentals() throws VideoException{
+		Account tomAccount = new Account("Tom", "Murphy", "tMurphy@gmail.com");
+		Video braveHeart = new Video("Brave Heart", 1995);
+		assertEquals(tomAccount.getOpenRentals().size(), 0);
 	}
 }
