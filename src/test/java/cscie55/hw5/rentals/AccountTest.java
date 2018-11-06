061fe37e0cbb5cb2310e0412007059cbcd39ed61
Brendan Murphy <brendanj.murphy@libertymutual.com>
2018-11-06 13:43:23 -0500

    more unit tests

--- src/test/java/cscie55/hw5/rentals/AccountTest.java
+++ src/test/java/cscie55/hw5/rentals/AccountTest.java
@@ -23,32 +23,79 @@ public class AccountTest {
 		Account mikeAccount = new Account("Mike", "Murphy", "mMurphy@gmail.com");
 		Video forrestGump = new Video("Forrest Gump", 1995);
 		LocalDate dueDate = LocalDate.of(2018, 11, 20);
-		VideoRental rental = new VideoRental(forrestGump, mikeAccount,dueDate);
-		assertEquals(mikeAccount.getOpenRentals().size(), 1);
+		new VideoRental(forrestGump, mikeAccount,dueDate);
+		assertEquals( 1, mikeAccount.getOpenRentals().size());
 	}
 
 	//tests that hasOpenRental returns false with no rentals
 	@Test
 	public void testNoOpenRentals() throws VideoException{
 		Account tomAccount = new Account("Tom", "Murphy", "tMurphy@gmail.com");
-		Video braveHeart = new Video("Brave Heart", 1995);
-		assertEquals(tomAccount.getOpenRentals().size(), 0);
+		assertEquals( 0, tomAccount.getOpenRentals().size());
 	}
 
-	//tests that settleRentals adds the returned video to closed rentals list
+	//tests that settleRentals adds a returned video to closed rentals list
+	@Test
+	public void testSettleRentals() throws VideoException{
+		Account mikeAccount = new Account("Mike", "Murphy", "mMurphy@gmail.com");
+		LocalDate dueDate = LocalDate.of(2018, 11, 20);
+		Video forrestGump = new Video("Forrest Gump", 1995);
+		new VideoRental(forrestGump, mikeAccount,dueDate);
+		mikeAccount.settleRentals();
+		assertEquals( 1, mikeAccount.getClosedRentals().size());
+	}
+
+	//tests that settleRentals adds all returned video to closed rentals list
+	@Test
+	public void testSettleManyRentals() throws VideoException{
+		Account mikeAccount = new Account("Mike", "Murphy", "mMurphy@gmail.com");
+		LocalDate dueDate = LocalDate.of(2018, 11, 20);
+		Video forrestGump = new Video("Forrest Gump", 1995);
+		Video braveHeart = new Video("Brave Heart", 1995);
+		new VideoRental(forrestGump, mikeAccount,dueDate);
+		new VideoRental(braveHeart, mikeAccount,dueDate);
+		mikeAccount.settleRentals();
+		assertEquals( 2, mikeAccount.getClosedRentals().size());
+	}
+
+	//tests that settleRental adds the returned video to closed rentals list
 	@Test
 	public void testSettleRental() throws VideoException{
 		Account mikeAccount = new Account("Mike", "Murphy", "mMurphy@gmail.com");
 		LocalDate dueDate = LocalDate.of(2018, 11, 20);
 		Video forrestGump = new Video("Forrest Gump", 1995);
 		VideoRental rental = new VideoRental(forrestGump, mikeAccount,dueDate);
-		mikeAccount.settleRentals();
-		assertEquals(mikeAccount.getClosedRentals().size(), 1);
+		mikeAccount.settleRental(rental);
+		assertEquals( 1, mikeAccount.getClosedRentals().size());
+	}
+
+	//tests that settleRental does not function when wrong video passed in
+	@Test
+	public void testSettleRentalWrongVideo() throws VideoException{
+		Account patAccount = new Account("Patrick", "Murphy", "pMurphy@gmail.com");
+		LocalDate dueDate = LocalDate.of(2018, 11, 20);
+		Video forrestGump = new Video("Forrest Gump", 1995);
+		new Video("Brave Heart", 1995);
+		new VideoRental(forrestGump, patAccount,dueDate);
+		patAccount.settleRental("Brave Hear");
+		assertEquals( 0, patAccount.getClosedRentals().size());
+	}
+
+	//tests that clear history deletes outstanding rentals
+	@Test
+	public void testClearHistory() throws VideoException{
+		Account patAccount = new Account("Patrick", "Murphy", "pMurphy@gmail.com");
+		LocalDate dueDate = LocalDate.of(2018, 11, 20);
+		Video forrestGump = new Video("Forrest Gump", 1995);
+		VideoRental rental = new VideoRental(forrestGump, patAccount,dueDate);
+		patAccount.settleRental(rental);
+		patAccount.clearHistory();
+		assertEquals( 0, patAccount.getClosedRentals().size());
 	}
 
 	//tests that equals method returns false with different accounts
 	@Test
-	public void testEqualsNegative(){
+	public void testEqualsNegativeScenario(){
 		Account brendanAccount = new Account("Brendan", "Murphy", "bMurphy@gmail.com");
 		Account mikeAccount = new Account("Mike", "Murphy", "mMurphy@gmail.com");
 		assertFalse(brendanAccount.equals(mikeAccount));
@@ -56,7 +103,7 @@ public class AccountTest {
 
 	//tests that equals method returns true with same accounts
 	@Test
-	public void testEqualsPositive(){
+	public void testEqualsPositiveScenario(){
 		Account brendanAccount = new Account("Brendan", "Murphy", "bMurphy@gmail.com");
 		Account brendanAccountClone = new Account("Brendan", "Murphy", "bMurphy@gmail.com");
 		assertTrue(brendanAccount.equals(brendanAccountClone));
